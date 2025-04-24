c9a50df52a9bdf7ddfd09c5d3a17f7fd13917115
moppy626 <m_himenezumi@yahoo.co.jp>
2025-04-24 23:52:57 +0900

    2進数の8ビット配列をcharに変換する関数を作成

--- server.c
+++ server.c
@@ -10,9 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minitalk.h"
+
+/*
+	2進数の8ビット配列をcharに変換します
+*/
+char to_char(int *ary)
+{
+	int ret;
+	int class;
+
+	ret = 0;
+	class = 128;
+	while(class >= 1)
+	{
+		if (*ary)
+			ret += class;
+		printf("ary:%d, ret:%d\n", *ary, ret);
+		class /= 2;
+		ary++;
+	}
+	return (ret);
+}
 int main(int argc, char const *argv[])
 {
 	(void)argc;
 	(void)argv;
+
+	int test[8] = {0, 1, 1, 0, 0, 0, 0, 1};
+	char ret = to_char(test);
+	printf("%c\n", ret);
 	return 0;
 }
